18e56fb29c58d24738d38ace6b02a3dcd2572cef
natonyastewart <natonyastewart15@gmail.com>
2025-03-16 14:30:36 -0500

    Exercise 1

--- app/forms.py
+++ app/forms.py
@@ -1,1 +1,10 @@
-# Add any form classes for Flask-WTF here
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, FileField
+from flask_wtf.file import FileAllowed
+from wtforms.validators import DataRequired
+
+
+class MovieForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    description = TextAreaField("Description", validators=[DataRequired])
+    poster = FileField("Poster", validators=[FileAllowed(['jpg', 'jpeg', 'png'], "Only JPG, PNG and JPEG Files Allowed!")])
--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    port: 5173,
+    strictPort: true
   }
 })
